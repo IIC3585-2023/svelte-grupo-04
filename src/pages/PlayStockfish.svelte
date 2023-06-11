3da8843dd273e087fc6e1d8d4770280a21c62276
alfre <alfredo.medina@uc.cl>
2023-06-11 19:22:45 -0400

    dont merge

--- src/pages/PlayStockfish.svelte
+++ src/pages/PlayStockfish.svelte
@@ -2,29 +2,48 @@
     import Chessboard from "../components/Chessboard.svelte";
     import { Chess } from 'chess.js';
 
+
+    // import stockfish 
+    const stockfish = new Worker("stockfish.js");
+    stockfish.postMessage("uci");
+    stockfish.postMessage("ucinewgame");
+    stockfish.postMessage("position startpos");
+    
+    
     let currentFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    let chess = new Chess(currentFen);
-    const playerColor = chess.turn();
+
+    let game = new Chess(currentFen);
+
+    stockfish.onmessage = (event) => {
+      if (event.data.includes("bestmove")) {
+        const bestMove = event.data.split(" ")[1];
+        const move = game.move(bestMove);
+        console.log(move);
+        // chessboard.setPosition(chess.fen());
+      }
+      console.log(event.data);
+    };
 
 
-    async function getStockfishMove(fen) {
-        fen = fen.replaceAll(" ", "%20")
-        const response = await fetch(`https://www.chessdb.cn/cdb.php?action=querybest&board=${fen}`);
-        const data = await response.text();
-        const move = data.split(":")[1];
-        return move;
-	}
+
+    // async function getStockfishMove(fen) {
+    //     this.engineGame().prepareMove()
 
 
-    $ : {
-        let chess = new Chess(currentFen);
-        if (chess.turn() !== playerColor) {
-            getStockfishMove(currentFen).then((move) => {
-                chess.move(move);
-                currentFen = chess.fen();
-            })
-        }
-    }
+    //     fen = 
+
+
+    //     return move;
+	// }
+
+
+    // $ : {
+    //     game = new Chess(currentFen);
+    //     if (!game.isGameOver()) {
+    //         prepareMove();
+    //         currentFen = game.fen();
+    //     }
+    // }
 
 </script>
 
@@ -37,3 +56,10 @@
         margin: 40px 0;
     }
 </style>
+
+<!-- 
+
+        // fen = fen.replaceAll(" ", "%20")
+        // const response = await fetch(`https://www.chessdb.cn/cdb.php?action=querybest&board=${fen}`);
+        // const data = await response.text();
+        // const move = data.split(":")[1]; -->
