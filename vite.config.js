45220e4bce3ccbbbd5bafddc00e4d0dc41d020dc
alfre <alfredo.medina@uc.cl>
2023-06-11 21:30:25 -0400

    feat/stockfish

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), wasm()],
+  plugins: [svelte(), crossOriginIsolation(), wasm()],
   optimizeDeps: {
     include: ["stockfish", "chess.js"],
   },
