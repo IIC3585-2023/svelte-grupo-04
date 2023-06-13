c0816cb26b688d2305a31310cf9ae285bdd796d0
alfre <alfredo.medina@uc.cl>
2023-06-12 22:07:26 -0400

    add: overlay for winning or losing against stockfish

--- src/pages/PlayStockfish.svelte
+++ src/pages/PlayStockfish.svelte
@@ -6,13 +6,41 @@
 
   import stockfish_img from "../assets/stockfish_img.png";
   import wasm_img from "../assets/wasm.png";
+  import win_gif from "../assets/win.gif";
+  import loss_gif from "../assets/loss.gif";
+  import draw_gif from "../assets/draw.gif";
 
   let chessboard;
   let currentFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
   let game = new Chess(currentFen);
-  let history = game.history({ verbose: true });
+  let history = game.history();
   let playerColor = game.turn();
+  const textsOverlay = {
+    draw: {
+      title: "Draw!",
+      text: "The game ended in a draw. You can do better.",
+      src: draw_gif,
+    },
+    win: {
+      title: "Check Mate!",
+      text: "Congratulations, you won the game. But remember, this Stockfish does not have his real strength.",
+      src: win_gif,
+    },
+    loss: {
+      title: "Check Mate!",
+      text: "You lost the game. Do better next time.",
+      src: loss_gif,
+    },
+  };
+
+  let currentOverlayText = textsOverlay.loss;
+  let overlayShowing = false;
+
+  function restart() {
+    chessboard.restartGame();
+    overlayShowing = false;
+  }
 
   const stockfish = new Worker("stockfish.js");
 
@@ -42,6 +70,19 @@
     if (!game.isGameOver() && game.turn() !== playerColor) {
       prepareMove(game.fen());
     }
+
+    if (game.isGameOver()) {
+      if (game.isCheckmate()) {
+        if (game.turn() === playerColor) {
+          currentOverlayText = textsOverlay.loss;
+        } else {
+          currentOverlayText = textsOverlay.win;
+        }
+      } else if (game.isDraw()) {
+        currentOverlayText = textsOverlay.draw;
+      }
+      overlayShowing = true;
+    }
   }
 </script>
 
@@ -55,10 +96,17 @@
     </span>
     <img src={wasm_img} alt="wasm" width="40px" height="40px" />
   </div>
+  <div class="overlay">
+    <div class="overlay-content {overlayShowing ? '' : 'hidden'}">
+      <h3>{currentOverlayText.title}</h3>
+      <p>{currentOverlayText.text}</p>
+      <img src={currentOverlayText.src} alt="gif" class="overlay-gif" />
+    </div>
+  </div>
   <Chessboard bind:this={chessboard} bind:chess={game} bind:history />
   <div class="bottom-section">
-    <TableOfMoves bind:history />
-    <button on:click={chessboard.restartGame}>
+    <TableOfMoves {history} />
+    <button on:click={restart}>
       <i class="fas fa-undo" />
       Restart Game
     </button>
@@ -71,10 +119,47 @@
     background-color: #262421;
     padding: 20px;
     border-radius: 5px;
+    position: relative;
   }
 
   img {
     margin: 0 10px;
+  }
+
+  .overlay-gif {
+    margin: 20px 0;
+    margin-bottom: 30px;
+    height: 130px;
+    max-width: 200px;
+    object-fit: cover;
+  }
+
+  .overlay {
+    position: absolute;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    z-index: 1;
+    width: calc(100% - 40px);
+    aspect-ratio: 1/1;
+    pointer-events: none;
+  }
+
+  .overlay-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(72, 72, 72, 0.9);
+    border-radius: 1rem;
+    padding: 0 20px;
+    max-width: 250px;
+  }
+
+  .hidden {
+    display: none;
   }
 
   .name-opponent {
