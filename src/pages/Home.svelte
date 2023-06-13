1682a6c1d6762eb841658ae16c97612e85a4c654
alfre <alfredo.medina@uc.cl>
2023-06-13 13:07:09 -0400

    lil front

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,148 +1,174 @@
 <script>
-    import Puzzle from '../components/Puzzle.svelte'
+  import Puzzle from "../components/Puzzle.svelte";
 
-    let postsData = [];
+  let postsData = [];
 
-    const userId = parseInt(localStorage.getItem('userId'));
+  const userId = parseInt(localStorage.getItem("userId"));
+  console.log(userId);
+  let postsLiked = [];
+  if (userId) {
+    checkLikes(userId);
+  }
 
-    async function getPosts() {
-        const response = await fetch('https://sveltebackendv2.onrender.com/posts')
-        postsData = await response.json();
-        console.log(postsData)
-	}
-    getPosts();
+  async function getPosts() {
+    const response = await fetch("https://sveltebackendv2.onrender.com/posts");
+    postsData = await response.json();
+    // console.log(postsData)
+  }
+  getPosts();
 
-    async function checkLike(postId) {
-        const response = await fetch(`https://sveltebackendv2.onrender.com/posts/likes/${userId}`);
-        const likes = await response.json();
-        if (likes.includes(postId)){
-            const isLiked = true;
-            return true;
-        } else {
-            const isLiked = false;
-            return false;
-        }
+  async function checkLikes(userId) {
+    const response = await fetch(
+      `https://sveltebackendv2.onrender.com/posts/likes/${userId}`
+    );
+    const likes = await response.json();
+    if (response.status === 200) {
+      postsLiked = likes;
+    } else {
+      console.log("Error: " + response.status);
+      postsLiked = [];
     }
+  }
 
-    async function deleteLike(postId){
-        const response = await fetch(`https://sveltebackendv2.onrender.com/posts/${postId}/unlike`, {
+  async function deleteLike(postId) {
+    changeLikesShowing(postId, "down");
+    const response = await fetch(
+      `https://sveltebackendv2.onrender.com/posts/${postId}/unlike`,
+      {
         method: "POST",
         headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            "userId": userId
+          userId: userId,
         }),
-        });
-        console.log(response);
-        if (response.status === 200) {
-            console.log("unlike");    
-            const isLiked = false;
-            await getPosts();
-        } else {
-            console.log("error");
-        }
+      }
+    );
+    if (response.status === 200) {
+      console.log("disliked");
+    } else {
+      console.log("error");
+      changeLikesShowing(postId, "up");
     }
+  }
 
-    async function giveLike(postId) {
-        const response = await fetch(`https://sveltebackendv2.onrender.com/posts/${postId}/like`, {
+  async function giveLike(postId) {
+    changeLikesShowing(postId, "up");
+    const response = await fetch(
+      `https://sveltebackendv2.onrender.com/posts/${postId}/like`,
+      {
         method: "POST",
         headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            "userId": userId
+          userId: userId,
         }),
-        });
-        if (response.status === 200) {
-            console.log("like");    
-            const isLiked = true;
-            checkLike(userId);
-            await getPosts();
-        } else {
-            console.log("error");
-        }
+      }
+    );
+    if (response.status === 200) {
+      console.log("like");
+    } else {
+      console.log("error");
+      changeLikesShowing(postId, "down");
     }
+  }
+
+  function changeLikesShowing(postId, upOrDown) {
+    const likes = document.querySelector(`.likes-id-${postId}`);
+    const button = document.querySelector(`.likes-button-id-${postId}`);
+    if (upOrDown === "down") {
+      postsLiked.pop();
+      likes.innerHTML = (parseInt(likes.innerHTML) - 1).toString();
+      button.innerHTML = "Like";
+      return;
+    }
+    postsLiked.push(postId);
+    likes.innerHTML = (parseInt(likes.innerHTML) + 1).toString();
+    button.innerHTML = "Dislike";
+  }
+
+  function giveOrDeleteLike(postId) {
+    if (postsLiked.includes(postId)) {
+      deleteLike(postId);
+    } else {
+      giveLike(postId);
+    }
+  }
 
 </script>
 
 <main>
-    {#each postsData as post, index (post.id)}
+  {#each postsData as post, index (post.id)}
     <div class="container">
-        <h3>{post.username}</h3>
-        <p>{post.description}</p>
-        <Puzzle pgn={post.pgn} key={index.toString()} solution={post.puzzle.solution}/>
-        <div class="tags-container">
-            {#each post.tags as tag}
-                <span class="tag">{tag}</span>
-            {/each}
-            {#await checkLike(post.id)}
-                <p>Verificando...</p>
-            {:then isLiked}
-                {#if isLiked === true}
-                    <button on:click={() => deleteLike(post.id)}>
-                        <span class="tag-likes">{post.likes} Likes</span>
-                        Dislike
-                    </button>
-                {:else}
-                    <button on:click={() => giveLike(post.id)}>
-                        <span class="tag-likes">{post.likes} Likes</span>
-                        Like
-                    </button>
-                {/if}
-            {:catch error}
-                <p>Error: {error.message}</p>
-            {/await}
-        </div>
+      <h3>{post.username}</h3>
+      <p>{post.description}</p>
+      <span class="number-likes likes-id-{post.id}">{post.likes}</span>
+      <span>Likes</span>
+      <button class="likes-button-id-{post.id}" on:click={() => giveOrDeleteLike(post.id)} disabled={!userId}>
+        {#if postsLiked.includes(post.id)}
+          Dislike
+        {:else}
+          Like
+        {/if}
+      </button>
+      <Puzzle
+        pgn={post.pgn}
+        key={index.toString()}
+        solution={post.puzzle.solution}
+      />
+      <div class="tags-container">
+        {#each post.tags as tag}
+          <span class="tag">{tag}</span>
+        {/each}
+      </div>
     </div>
-    {/each}
+  {/each}
 </main>
 
-
 <style scoped>
-    main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-    }
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        background-color: #262421;
-        padding: 10px;
-        margin: 20px 0;
-        border-radius: 0.5rem;
-    }
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #262421;
+    padding: 10px;
+    margin: 20px 0;
+    border-radius: 0.5rem;
+  }
 
-    .tags-container {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-        gap: 5px;
-        flex-wrap: wrap;
-    }
+  .tags-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    flex-wrap: wrap;
+  }
 
-    .tag {
-        background-color: #333;
-        font-size: small;
-        border-radius: 5px;
-        padding: 5px;
-        margin: 10px 5px;
-    }
+  .tag {
+    background-color: #333;
+    font-size: small;
+    border-radius: 5px;
+    padding: 5px;
+    margin: 10px 5px;
+  }
 
-    .tag-likes {
-        background-color: #333;
-        font-size: small;
-        border-radius: 5px;
-        padding: 5px;
-        margin: 10px 5px;
-    }
-
+  .tag-likes {
+    background-color: #333;
+    font-size: small;
+    border-radius: 5px;
+    padding: 5px;
+    margin: 10px 5px;
+  }
 </style>
